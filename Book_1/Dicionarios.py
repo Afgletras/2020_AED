bda07a07c64b67f13abc0bb262fc13f43e62da2c
Afgletras <afgletras@gmail.com>
2020-03-08 11:23:41 +0000

    .

---
+++ Book_1/Dicionarios.py
@@ -0,0 +1,11 @@
+# É uma coleção de pares de itens, em que a cada item corresponde uma chave
+
+capitals = {'Iowa': 'DesMoines', 'Winsconsin':'Madison'}
+print(capitals)
+
+# Para adicionar mais items ao dicionário existe várias formas, a mais simples é:
+
+capitals['Utah'] = 'SaltLakeCity'
+print(capitals)
+capitals['California'] = 'Sacramento'
+print(capitals)
