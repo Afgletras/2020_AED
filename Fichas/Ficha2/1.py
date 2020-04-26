c38dc2bcd987e521e94bd4612433b7468eb1c2d6
Afgletras <afgletras@gmail.com>
2020-04-26 21:34:21 +0100

    Ficha 2

--- Fichas/Ficha2/1.py
+++ Fichas/Ficha2/1.py
@@ -1,21 +1,20 @@
 import random
 
 # O(n^2)
-lista = []
-for x in range(10):
-   lista.append(random.randint(0,9))
-   min = lista[0]
-   for i in range(len(lista)):
-       if lista[i] < min:
-           min = lista[i]
+lista = [2, 5, 6, 9, 0, 2, 4, 3, 1, 9]
+min = lista[0]
+for i in range(len(lista)):
+    if lista[i] < min:
+        min = lista[i]
 
-print("O número mais pequeno da lista ", lista, "é ", min)
+print("O(n^2) - O número mais pequeno da lista ", lista, "é ", min)
+
 
 # O(n)
-lista2 = [1, 4, 7, 9, 3, 2, 8, 0]
-min2 = lista2[0]
-for i in range(len(lista2)):
-    if lista2[i] < min2:
-        min2 = lista2[i]
+lista2 = []
+for x in range(10):
+   lista2.append(random.randint(0,9))
 
-print("O número mais pequeno da lista ", lista2, "é ", min2)
+list.sort(lista2)
+print("O(n) - O número mais pequeno da lista ", lista2, "é ", lista2[0])
+
