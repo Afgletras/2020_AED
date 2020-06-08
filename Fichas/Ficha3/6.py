d725fda1cf72a023d3a3cd665520e5baa573d741
Afgletras <afgletras@gmail.com>
2020-06-08 13:56:43 +0100

    Ficha 3 - Completa (Confirmar ex6)

--- Fichas/Ficha3/6.py
+++ Fichas/Ficha3/6.py
@@ -4,12 +4,24 @@ def factorial(n):
 
     return n * factorial(n-1)
 
-#def entry_pascalTriangle(i, j):
+# Função que devolve o valor da formula de a(i,j)
+def entry_pascalTriangle(row, column):
+    if (column == 0) or (row == column):
+        return 1
+    else:
+        print(int(factorial(row) / (factorial(column) * factorial(row - column))))
 
+# Função que devolve apenas uma linha
+def print_line_pascalTriangle(i):
+    if i < 0:
+        return
+    else:
+        line = []
+        for j in range(i + 1):
+            line.append(int(factorial(i) / (factorial(j) * factorial(i - j))))
+    print(line)
 
-#def print_line_pascalTriangle(i):
-
-# Funções que escrevem no ecrã o triangulo de Pascal
+# Função que escreve no ecrã o triangulo de Pascal
 def print_pascal_triangle(n):
     if n < 0:
         return
@@ -21,8 +33,17 @@ def print_pascal_triangle(n):
     print(line)
 
 def main():
-    n = int(input("Introduza o valor de n (número de linhas): "))
+    n = int(input("Introduza o valor de n: "))
+    i = int(input("Introduza o número da linha: "))
+    j = int(input("Introduza o número da coluna: "))
+
+    print("Triângulo com ", n, " linhas:")
     print_pascal_triangle(n)
 
+    print("Linha numero", n, "do triângulo:")
+    print_line_pascalTriangle(n)
+
+    print("Valor de a(", i, ",", j, "):")
+    entry_pascalTriangle(i, j)
 
 main()
